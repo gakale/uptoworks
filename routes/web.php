bfe601ba7169252dbb0dbeb12e1f5156ab5af7af
gnakale <gnakaleroland@gmail.com>
2021-09-20 16:42:36 +0000

    conifgration de fortify

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/freelance',function (){
+
+    return view('freelance');
+})->middleware('auth')->name('freelance');
